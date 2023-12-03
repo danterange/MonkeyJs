ece99037dcca4c6d8eea86b7ed00e350ee6526a7
Your Name <test11>
2023-12-04 00:50:29 +0800

    temp.js

---
+++ temp.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Userscript1111
+// @namespace    test222
+// @version      0.1
+// @description  油猴脚本测试
+// @author       dante
+// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Hello World")
+    // Your code here...
+})();
+
+
+
