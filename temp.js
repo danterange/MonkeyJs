8f3907f3fd805608b81269d821ad08c619fb7e9d
Your Name <test11>
2023-12-04 00:58:12 +0800

    aaa

--- temp.js
+++ temp.js
@@ -7,6 +7,7 @@
 // @match        https://greasyfork.org/zh-CN/script_versions/new
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
